8e942cf43f5eb091d3f84ad9ba2ec3a170e79bed
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2023-11-30 00:30:26 +0200

    hw-4

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,28 @@
-function slugify(title) {
-  const normalizeTitle = title.toLowerCase();
-  return normalizeTitle.split(' ').join('-')
+function isEnoughCapacity(products, containerSize) {
+  const arrQuntity = Object.values(products);
+  let totalQuntity = 0;
+  for(let i = 0; i < arrQuntity.length; i++) {
+    totalQuntity += arrQuntity[i];
+  }
+  if(totalQuntity <= containerSize) {
+    return true
+  } else {
+    return false
+  }
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
